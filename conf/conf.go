91ee4cd4e1cf1088f15c17942a4df8b1bb9c7f37
afreto <kongandmarx@163.com>
2019-07-31 17:41:24 +0800

    chore

--- conf/conf.go
+++ conf/conf.go
@@ -27,26 +27,9 @@ func init() {
 		viper.SetDefault(k, v)
 	}
 
-	//configType := "yaml"
-	//defaultPath := "conf"
-	//baseConfig := viper.New()
-	//baseConfig.SetConfigName("app")
-	//baseConfig.AddConfigPath(defaultPath)
-	//baseConfig.SetConfigType(configType)
-	//err := baseConfig.ReadInConfig()
-	//if err != nil {
-	//	//panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	//	panic(err)
-	//	return
-	//}
-	//config := baseConfig.AllSettings()
-	//for k, v := range config {
-	//	viper.SetDefault(k, v)
-	//}
-	//
 	env := os.Getenv("ENV")
 	if env != "" {
-		envConfig, err := box.Find(env+".yaml")
+		envConfig, err := box.Find(env + ".yaml")
 		if err != nil {
 			panic(err)
 			return
@@ -57,23 +40,4 @@ func init() {
 			panic(err)
 		}
 	}
-	//fmt.Println(env)
-	//if env != "" {
-	//	viper.SetConfigName("loc")
-	//	viper.AddConfigPath("conf")
-	//	//viper.SetConfigType(configType)
-	//	err = viper.ReadInConfig()
-	//	if err != nil {
-	//		panic(err)
-	//		return
-	//	}
-	//} else {
-	//	fmt.Println("env is empty")
-	//	return
-	//}
-	//err = viper.ReadInConfig()
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
 }
